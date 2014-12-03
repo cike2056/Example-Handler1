78af597b5437468fe2d9a24e1b274a4748c87d9e
cike2056 <whzhn1990@163.com>
2014-12-03 18:56:19 +0800

    主线程中加载耗时操作,(没有控件,空白),或是卡死. message.arg1=i;/**内容*/ message.obj="start";/**多类型内容*/ message.what=1;/**标识*/

--- src/com/qf/example_handler1/MainActivity.java
+++ src/com/qf/example_handler1/MainActivity.java
@@ -2,33 +2,68 @@ package com.qf.example_handler1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ProgressBar;
+import android.widget.TextView;
+/**主线程中加载耗时.
+ * (没有控件,空白)卡死,或是崩溃.
+ * @author uaige
+ *
+ */
 public class MainActivity extends Activity {
-
+	private ProgressBar pb;
+	private TextView tx;
+	private Handler handler = new Handler(){
+		public void handleMessage(android.os.Message msg) {
+			msg.what=1;//标示
+			pb.setProgress(msg.arg1);
+			tx.setText(""+msg.arg1+"%");
+		};
+	};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initView();
+		setTxProgress();
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	
+	/**
+	 * 初始化界面.
+	 */
+	private void initView() {
+		// TODO Auto-generated method stub
+		pb = (ProgressBar) findViewById(R.id.pb);
+		tx =(TextView) findViewById(R.id.tx);
 	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+	/**
+	 * 设置Progress
+	 */
+	private void setTxProgress() {
+		// TODO Auto-generated method stub
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				for(int i=0;i<100;i++){
+					try {
+						Thread.sleep(1000);
+//						pb.setProgress(i);
+//						tx.setText(""+i+"%");
+						Message message = handler.obtainMessage();
+						message.arg1=i;/**内容*/
+						message.obj="start";/**多类型内容*/
+						message.what=1;/**标识*/
+						handler.sendMessage(message);
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+		
 	}
 }
